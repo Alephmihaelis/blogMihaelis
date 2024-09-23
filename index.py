774d8e3f987beba6ed804a607dc44b1d968d1c3b
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-09-23 15:34:54 -0300

    att

--- index.py
+++ index.py
@@ -3,9 +3,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+# Rota para a página inicial
 @app.route('/')
 def home():
+    htmlparams = {
+        'title': 'Página inicial',
+        'css': '/static/css/home.css',
+        'js': '/static/js/home.js'
+    }
     return 'Hello World!'
 
 
