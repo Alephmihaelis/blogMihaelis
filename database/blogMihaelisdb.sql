9c2cb92229b0a6455591b5fbcb93bef1f9e450cb
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-09-23 13:41:25 -0300

    att

--- database/blogMihaelisdb.sql
+++ database/blogMihaelisdb.sql
@@ -1,4 +1,3 @@
-
 -- -------------------------------- --
 -- Banco de dados 'blogMihaelis' --
 -- -------------------------------- --
@@ -32,4 +31,41 @@ CREATE DATABASE blogMihaelis
         sta_description VARCHAR(255),
         sta_type ENUM('moderator', 'author', 'admin') DEFAULT 'moderator',
         sta_status ENUM('on', 'off', 'del') DEFAULT 'on'
+    );
+
+    CREATE TABLE article (
+        art_id INT PRIMARY KEY AUTO_INCREMENT,
+        art_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        art_title VARCHAR(127) NOT NULL,
+        art_resume VARCHAR(255) NOT NULL,
+        art_thumbnail VARCHAR(255) NOT NULL,
+        art_content TEXT NOT NULL,
+        art_view INT DEFAULT 0,
+        art_author INT,
+        art_status ENUM('on', 'off', 'del') DEFAULT 'on',
+        -- Chave estrangeira para 'staff' 
+        FOREIGN KEY (art_author) REFERENCES staff (sta_id)
+    );
+
+    CREATE TABLE comment (
+        com_id INT PRIMARY KEY AUTO_INCREMENT,
+        com_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        com_article INT NOT NULL,
+        com_author_name VARCHAR(127) NOT NULL,
+        com_author_email VARCHAR(255) NOT NULL,
+        com_comment TEXT NOT NULL,
+        com_status ENUM('on', 'off', 'del') DEFAULT 'on',
+        -- Chave estrangeira para 'article'
+        FOREIGN KEY (com_article) REFERENCES article (art_id)
+    );
+
+    -- Cria a tabela 'contact'
+    CREATE TABLE contact (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        name VARCHAR(127) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        subject VARCHAR(255) NOT NULL,
+        message TEXT,
+        status ENUM('received', 'responded', 'deleted') DEFAULT 'received'
     );
