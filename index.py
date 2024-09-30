d8ad291e41f2ee8787c8bc6fbc3041c76970959f
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-09-30 13:31:11 -0300

    att

--- index.py
+++ index.py
@@ -1,55 +1,62 @@
-
 # Importa as dependências do aplicativo
 from flask import Flask, render_template
 
 # Constantes do site
-SITE = {    
-     'NAME': 'blogMiha',
-     'OWNER': '© Miha',
-     'LOGO': '/static/img/logo.png'
+SITE = {
+    'name': 'MihaBlog',
+    'owner': 'Mihael',
+    'logo': '/static/img/logo.png',
+    'favicon': '/static/img/logo.png'
 }
 
 # Cria uma aplicação Flask usando uma instância do Flask
 app = Flask(__name__)
 
+
 ######################
 # Rotas da aplicação #
 ######################
 
-# Rota para a página inicial → raiz
-@app.route('/')
+@app.route('/')  # Rota para a página inicial → raiz
 def home():
     # Passa parâmetros para o template
-    # CSS e JS são opcionais
-    page = {
+    # 'css' e 'js' são opcionais
+    toPage = {
+        # Título da página → <title></title>
         'site': SITE,
-        'title': '', # Título da página → <title></title>
-        'css': 'home.css', # Folhas de estilo desta página (opcional)
-        'js': 'home.js', # JavaScript desta página (opcional)
+        'title': '',
+        'css': 'home.css',  # Folhas de estilo desta página (opcional)
+        'js': 'home.js',  # JavaScript desta página (opcional)
         # Outras chaves usadas pela página
-        'coisas': ('Casa', 'Carro', 'Moto', 'Peteca')
+        'coisas': ('casa', 'carro', 'moto', 'peteca')
     }
-    # Renderizar = fazer a conversão do código Python em código HTML.
-    return render_template('home.html', page=page)
 
-@app.route('/contacts') # Rota para a página de contatos → /contacts
+    # Renderiza template passando a variável local `toPage`
+    # para o template como `page`.
+    return render_template('home.html', page=toPage)
+
+
+@app.route('/contacts')  # Rota para a página de contatos → /contacts
 def contacts():
-        page = {
+
+    toPage = {
         'site': SITE,
-        'title': 'Contato',
+        'title': 'Faça contato',
         'css': 'home.css'
     }
-        return render_template('contacts.html', page=page)
+
+    return render_template('contacts.html', page=toPage)
+
 
 @app.route('/about')
 def about():
-     page = {
-          'site': SITE,
-          'title': 'Sobre nós',
-          'css': 'about.css',
-          'js': 'about.js'
-     }
-     return render_template('about.html', page=page)
+    toPage = {
+        'site': SITE,
+        'title': 'Sobre',
+        'css': 'about.css'
+    }
+
+    return render_template('about.html', page=toPage)
 
 
 if __name__ == '__main__':
