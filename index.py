e3d9f6620905a1119bf5b8e61a2f28795eb4caa1
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-09-30 14:22:22 -0300

    att

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 # Importa as dependências do aplicativo
 from flask import Flask, render_template
+from flask_mysqldb import MySQL, MySQLdb
 
 # Constantes do site
 SITE = {
@@ -12,6 +13,16 @@ SITE = {
 # Cria uma aplicação Flask usando uma instância do Flask
 app = Flask(__name__)
 
+# Configurações de acesso ao MySQL
+app.config['MYSQL_HOST'] = 'localhost'  # Servidor do MySQL
+app.config['MYSQL_USER'] = 'root'       # Usuário do MySQL
+app.config['MYSQL_PASSWORD'] = ''       # Senha do MYSQL
+app.config['MYSQL_DB'] = 'mihablogdb'   # Nome do banco de dados
+
+# Variável de conexão com o MySQL
+mysql = MySQL(app) # Função sempre começam com letras minúsculas (como variáveis, etc.)
+                    # Mas toda classe começa com letra maiúscula
+                    # J. C. Martin, Código limpo (livro)
 
 ######################
 # Rotas da aplicação #
@@ -19,6 +30,31 @@ app = Flask(__name__)
 
 @app.route('/')  # Rota para a página inicial → raiz
 def home():
+
+    # Consulta SQL
+    sql = '''
+    -- Seleciona os campos abaixo
+    SELECT art_id, art_title, art_resume, art_thumbnail
+    -- desta tabela
+    FROM article
+    -- art status é 'on'
+    WHERE art_status = 'on'
+        -- e arte_date é menor ou igual à data atual
+        -- não obtém artigos com data futura (agendados)
+        AND art_date <= NOW()
+    -- ordena pela ordem mais recente
+    ORDER BY art_date DESC;
+    '''
+
+    # Executa a query e obtém os dados na forma de DICT
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute(sql)
+    articles = cur.fetchall()
+    cur.close()
+
+    # Somente para debug
+#    print('\n\n\n', articles, '\n\n\n')
+
     # Passa parâmetros para o template
     # 'css' e 'js' são opcionais
     toPage = {
@@ -28,11 +64,10 @@ def home():
         'css': 'home.css',  # Folhas de estilo desta página (opcional)
         'js': 'home.js',  # JavaScript desta página (opcional)
         # Outras chaves usadas pela página
-        'coisas': ('Casa', 'Carro', 'Moto', 'Peteca')
+        'articles': articles
     }
 
-    # Renderiza template passando a variável local `toPage`
-    # para o template como `page`.
+    # Renderiza template passando a variável local `toPage` para o template como `page`.
     return render_template('home.html', page=toPage)
 
 
